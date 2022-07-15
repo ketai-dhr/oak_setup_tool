7764504a1006ff1fa6df33ae0f0c33e7aed3a6ff
ketai <1057857271@qq.com>
2022-07-15 13:16:25 +0800

    many update

--- depthai_calibrate.bat
+++ depthai_calibrate.bat
@@ -1,7 +1,5 @@
 @echo off
 
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-cd /d %~dp0
 start .\depthai\depthai_demo_python\python.exe .\depthai\calibrate.py -s 2.5 -db -brd BW1098OBC
 
 exit
